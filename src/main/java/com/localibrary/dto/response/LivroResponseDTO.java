b7232f21a8b5a91c87f97deb003a563afb4aefd3
RafaelSR0x <raffssantana007@gmail.com>
2025-12-09 19:04:22 -0300

    Resumo adicionado ao LivroResponseDTO

--- src/main/java/com/localibrary/dto/response/LivroResponseDTO.java
+++ src/main/java/com/localibrary/dto/response/LivroResponseDTO.java
@@ -9,11 +9,13 @@ public class LivroResponseDTO {
     private String titulo;
     private String autor;
     private String capa;
+    private String resumo;
 
     public LivroResponseDTO(Livro livro) {
         this.id = livro.getId();
         this.titulo = livro.getTitulo();
         this.autor = livro.getAutor();
         this.capa = livro.getCapa();
+        this.resumo = livro.getResumo();
     }
 }
